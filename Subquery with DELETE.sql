2520d09c3b68fa193653f7d26d01b14b96bc8794
Shubham Singh <111279439+ShubhamSingh-9@users.noreply.github.com>
2023-10-19 15:02:47 +0100

    Delete Subquery with DELETE.sql

--- Subquery with DELETE.sql
+++
@@ -1,9 +0,0 @@
--- SUbquery using DELETE
--- DELETE all the customers record who have never ordered 
-
-USE zomato;
-
-
-DELETE FROM users
-WHERE user_id IN(SELECT user_id from users
-where user_id NOT IN (SELECT DISTINCT(user_id) FROM orders))
