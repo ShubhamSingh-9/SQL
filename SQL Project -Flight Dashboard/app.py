3d77c36dcac1d07729c51aaf54cabafb7fa4bd32
Shubham Singh <111279439+ShubhamSingh-9@users.noreply.github.com>
2023-11-02 14:35:57 +0000

    Add files via upload

---
+++ SQL Project -Flight Dashboard/app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from dbhelper import DB
+
+db = DB()
+
+st.sidebar.title('Flights Analytics')
+user_option = st.sidebar.selectbox('Menu',['Select One','Check Flights','Analytics'])
+
+if user_option == 'Check Flights':
+    st.title('Check Flights')
+    col1,col2 = st.columns(2)
+    city = db.fetch_city_names()
+    with col1:
+        st.selectbox('Source',sorted(city))
+    with  col2:
+        st.selectbox('Destination', sorted(city))
+
+        if st.button('Search'):
+            pass
+elif user_option == 'Analytics':
+    st.title('Analytics')
+else:
+    st.title('Tell about the Project')
